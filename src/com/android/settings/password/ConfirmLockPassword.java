f4e7f3613934bca4e3e8b4e7cc7331d40599cd36
Michael Bestas <mikeioannina@gmail.com>
2013-12-12 05:40:54 +0200

    Settings: Forward port lock pattern grid size (2/2)
    Author: d34d <clark@cyngn.com>
    Date:   Tue Dec 29 11:51:21 2015 -0800
    
        Fingerprint: Forward challenge extras
    
        The forward port of custom pattern sizes introduced a bug that would
        cause an NPE when trying to add a fingerprint for security. This patch
        forwards the extras associated with the challenge that would have
        normally been passed directly in to the ChooseLockPattern fragment.
    
        Change-Id: I3ad994b2604ff45b573c011c3588afe8b35bfdd3
        TICKET: CYNGNOS-1490
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Mon Mar 21 15:17:16 2016 -0700
    
        Settings: forward decrypt required on boot flag
    
        This wasn't being pass forward and all of the defaults had it set to
        true.
    
        Ticket: CYNGNOS-2270
    
        Change-Id: I90a6c918bb5b4b52791bcff18a6ea3bcfb194547
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Fri Dec 11 11:37:00 2015 -0600
    
        Settings: allow rotation while setting new pattern
    
        Change-Id: I0fc9a7df686b8af0c60edf1916dc6fe02185f704
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Mon Apr 6 12:20:33 2015 -0700
    
        Settings: handle decrypting larger pattern sizes
    
        Change-Id: Id24d46829063171fa87cabb23a7da378726d7548
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Tue Apr 28 14:02:21 2015 -0700
    
        CryptKeeper: pattern unlock displays incorrect pw when correct
    
        fakeUnlockAttempt() gets called when the user inputs any pattern length
        < 4 which queues up the 'incorrect error' message. The message needs to
        be cleared before trying to actually check the password so it never goes
        through in case the password was correct.
    
        Change-Id: If78db332d3d696ba443d0be911fb5db504cb14cd
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Fri May 1 17:07:47 2015 -0700
    
        Settings: fix non lock pattern CryptKeeper crash
    
        Change-Id: Ib2d6c9a468bfe778d5cb927759f11b2b03c25ee6
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Tue May 19 14:06:51 2015 -0700
    
        CryptKeeper: layout whole screen in bounds
    
        Add flags to make the screen layout properly on devices with the
        navigation bar visible
    
        Change-Id: I8607c8e49ac3625a7fb6a13b4501ea7ac818b9ba
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Wed May 20 10:31:13 2015 -0700
    
        CryptKeeper improvements
    
        - Status text was used enough to warrant it being a field variable
          instead of looking for the view every time
    
        - Display proper text after changing pattern sizes (to input a pattern,
          not a password)
    
        - Disable changing pattern sizes while validaing pattern
    
        REFS: LETTUCE-557, LETTUCE-352
        Change-Id: Ib4ecf04a58da8d1648d514d9650d69c33d9587a7
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Alberto97 <albertop2197@gmail.com>
    Date:   Mon Nov 21 20:47:25 2016 +0100
    
        Settings: Use GLIF Theme for missing Settings > Screen lock Activities
    
        Icons by Asher
    
        Change-Id: Icf1627b41ef604302a5819ad3b1bdfd6d8479202
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Mon May 10 22:00:01 2021 +0200
    
        Settings: Remove hardcoded extras from ChooseLockPatternSize
    
        Change-Id: I860d87842de0dbb9f87394a0c1fc5043fd16b07b
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Tue May 11 11:32:38 2021 +0200
    
        Settings: Make setup wizard go through ChooseLockPatternSize if needed
    
        Change-Id: Idfd3670d4b541b3618c4416b3b9dbf7d88ae3ed1
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Mon May 10 22:29:49 2021 +0200
    
        Settings: Make FRP go through ChooseLockPatternSize if needed
    
        Change-Id: I70b0ddb20033dd5505c391763a0a58fd2f9c8004
    
    Change-Id: I7078d703c218cd096d9b77c003a94b52fbce6322

--- src/com/android/settings/password/ConfirmLockPassword.java
+++ src/com/android/settings/password/ConfirmLockPassword.java
@@ -654,7 +654,8 @@ public class ConfirmLockPassword extends ConfirmDeviceCredentialBaseActivity {
                                 mLockPatternUtils,
                                 mRemoteLockscreenValidationFragment.getLockscreenCredential(),
                                 /* currentCredential= */ null,
-                                mEffectiveUserId);
+                                mEffectiveUserId,
+                                mLockPatternUtils.getLockPatternSize(mEffectiveUserId));
                     } else {
                         mCredentialCheckResultTracker.setResult(/* matched= */ true, new Intent(),
                                 /* timeoutMs= */ 0, mEffectiveUserId);
--- src/com/android/settings/password/ConfirmLockPattern.java
+++ src/com/android/settings/password/ConfirmLockPattern.java
@@ -118,6 +118,7 @@ public class ConfirmLockPattern extends ConfirmDeviceCredentialBaseActivity {
         private DisappearAnimationUtils mDisappearAnimationUtils;
 
         private boolean mIsManagedProfile;
+        private byte mPatternSize;
 
         // required constructor for fragments
         public ConfirmLockPatternFragment() {
@@ -144,6 +145,7 @@ public class ConfirmLockPattern extends ConfirmDeviceCredentialBaseActivity {
             mSudContent.setPadding(mSudContent.getPaddingLeft(), 0, mSudContent.getPaddingRight(),
                     0);
             mIsManagedProfile = UserManager.get(getActivity()).isManagedProfile(mEffectiveUserId);
+            mPatternSize = mLockPatternUtils.getLockPatternSize(mEffectiveUserId);
 
             // make it so unhandled touch events within the unlock screen go to the
             // lock pattern view.
@@ -158,6 +160,7 @@ public class ConfirmLockPattern extends ConfirmDeviceCredentialBaseActivity {
                 mDetailsText = intent.getCharSequenceExtra(
                         ConfirmDeviceCredentialBaseFragment.DETAILS_TEXT);
                 mCheckBoxLabel = intent.getCharSequenceExtra(KeyguardManager.EXTRA_CHECKBOX_LABEL);
+                mPatternSize = intent.getByteExtra("pattern_size", mPatternSize);
             }
             if (TextUtils.isEmpty(mHeaderText) && mIsManagedProfile) {
                 mHeaderText = mDevicePolicyManager.getOrganizationNameForUser(mUserId);
@@ -165,6 +168,7 @@ public class ConfirmLockPattern extends ConfirmDeviceCredentialBaseActivity {
 
             mLockPatternView.setInStealthMode(!mLockPatternUtils.isVisiblePatternEnabled(
                     mEffectiveUserId));
+            mLockPatternView.setLockPatternSize(mPatternSize);
             mLockPatternView.setOnPatternListener(mConfirmExistingLockPatternListener);
             mLockPatternView.setOnTouchListener((v, event) -> {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
@@ -506,14 +510,15 @@ public class ConfirmLockPattern extends ConfirmDeviceCredentialBaseActivity {
 
             }
 
-            public void onPatternDetected(List<LockPatternView.Cell> pattern) {
+            public void onPatternDetected(List<LockPatternView.Cell> pattern, byte patternSize) {
                 if (mPendingLockCheck != null || mDisappearing) {
                     return;
                 }
 
                 mLockPatternView.setEnabled(false);
 
-                final LockscreenCredential credential = LockscreenCredential.createPattern(pattern);
+                final LockscreenCredential credential = LockscreenCredential.createPattern(pattern,
+                        patternSize);
 
                 if (mRemoteValidation) {
                     validateGuess(credential);
@@ -652,7 +657,8 @@ public class ConfirmLockPattern extends ConfirmDeviceCredentialBaseActivity {
                                 mLockPatternUtils,
                                 mRemoteLockscreenValidationFragment.getLockscreenCredential(),
                                 /* currentCredential= */ null,
-                                mEffectiveUserId);
+                                mEffectiveUserId,
+                                mLockPatternUtils.getLockPatternSize(mEffectiveUserId));
                     } else {
                         mCredentialCheckResultTracker.setResult(/* matched= */ true, new Intent(),
                                 /* timeoutMs= */ 0, mEffectiveUserId);
