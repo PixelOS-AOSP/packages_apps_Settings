391a0a77aefba514557b95af91d969978abd6e73
Chris Lahaye <dev@chrislahaye.com>
2017-02-21 17:33:16 -0500

    Settings: Introduce pocket judge [2/2]
    AICP Pie: Modified defaults to false.
    	Brought up to 10 with androidx
    
    @jhenrique09 edits: Adapt strings, and also use config_pocketModeSupported on isAvailable
    
    Change-Id: I398c8f0be23c27cbcf1fb57eed44a801b31bc6c9
    
    Settings: Remove pocket lock toggle icons
    
     * Also move it to lockscreen settings
    
    Change-Id: Iae8a6b4739c317e723c0cfad46db5ceb494f74f8
    
    Co-authored-by: Fabian Leuteneegger <fabian.leutenegger@bluewin.ch>
    Change-Id: I91ce513da3a88123218d95235da1cd57d7fd6c4d

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -25,6 +25,7 @@ import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.display.BrightnessLevelPreferenceController;
 import com.android.settings.display.CameraGesturePreferenceController;
 import com.android.settings.display.LiftToWakePreferenceController;
+import com.android.settings.display.PocketJudgePreferenceController;
 import com.android.settings.display.ScreenSaverPreferenceController;
 import com.android.settings.display.ShowOperatorNamePreferenceController;
 import com.android.settings.display.TapToWakePreferenceController;
@@ -88,6 +89,7 @@ public class DisplaySettings extends DashboardFragment {
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
         controllers.add(new CameraGesturePreferenceController(context));
         controllers.add(new LiftToWakePreferenceController(context));
+        controllers.add(new PocketJudgePreferenceController(context));
         controllers.add(new ScreenSaverPreferenceController(context));
         controllers.add(new TapToWakePreferenceController(context));
         controllers.add(new VrDisplayPreferenceController(context));
