393b641fe4778b3bcbe4d6a5d1528a2aa73e4d0f
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-01-03 03:38:03 +0000

    Settings: Append custom version on build display id
    Change-Id: I831800ae0a769513b6b0c008ba492cd8334b3b65
    Signed-off-by: Karan Parashar <whyredfire@gmail.com>

--- src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
+++ src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
@@ -79,7 +79,14 @@ public class BuildNumberPreferenceController extends BasePreferenceController im
 
     @Override
     public CharSequence getSummary() {
-        return BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY);
+        StringBuilder sb = new StringBuilder();
+        sb.append(BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY));
+        String customVersion = VersionUtils.getcustomVersion();
+        if (!customVersion.equals("")){
+            sb.append("\n");
+            sb.append(customVersion);
+        }
+        return sb.toString();
     }
 
     @Override
