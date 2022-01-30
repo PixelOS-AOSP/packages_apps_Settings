9064c9f86f9c2a5568a723bfd1f514bdf667e335
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2022-01-30 03:43:39 -0500

    Settings: Move over refresh rate controls to Display
    * Move these preference to a completely separate category.
    
    * Don't block them if the user does not have developer options
      enabled.
    
    [bheatleyyy]
     - Retain show refresh rate preference in developer options
    
    Change-Id: Ic67b32b949a3c5b4ff280449580016bf4d9b9d6b
    Signed-off-by: bheatleyyy <sugakesshaa@gmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -27,6 +27,7 @@ import com.android.settings.display.LiftToWakePreferenceController;
 import com.android.settings.display.ScreenSaverPreferenceController;
 import com.android.settings.display.ShowOperatorNamePreferenceController;
 import com.android.settings.display.TapToWakePreferenceController;
+import com.android.settings.display.ForcePeakRefreshRatePreferenceController;
 import com.android.settings.display.ThemePreferenceController;
 import com.android.settings.display.VrDisplayPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -83,6 +84,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new LiftToWakePreferenceController(context));
         controllers.add(new ScreenSaverPreferenceController(context));
         controllers.add(new TapToWakePreferenceController(context));
+        controllers.add(new ForcePeakRefreshRatePreferenceController(context));
         controllers.add(new VrDisplayPreferenceController(context));
         controllers.add(new ShowOperatorNamePreferenceController(context));
         controllers.add(new ThemePreferenceController(context));
