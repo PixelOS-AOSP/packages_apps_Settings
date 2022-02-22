7d2e75f66e6f5e104c48b59a31b462f451548769
bheatleyyy <sugakesshaa@gmail.com>
2022-02-22 17:04:16 +0530

    Revert recent changes done to smooth display
    Revert "fixup!: bring back smooth display"
    
    This reverts commit f7d5be5831f1e820f9523dc02ff3559b80f6579c.
    
    Revert "Revert "Settings: Allow devices to disable minimum refresh rate""
    
    This reverts commit 91be17269bf15e5dfbb3478fb6aa85141522f388.

--- src/com/android/settings/fuelgauge/batterysaver/BatterySaverRefreshRatePreferenceController.java
+++ src/com/android/settings/fuelgauge/batterysaver/BatterySaverRefreshRatePreferenceController.java
@@ -44,7 +44,7 @@ public class BatterySaverRefreshRatePreferenceController extends TogglePreferenc
 
     @Override
     public int getAvailabilityStatus() {
-        return mContext.getResources().getBoolean(R.bool.config_show_smooth_display)
+        return mContext.getResources().getBoolean(R.bool.config_show_refresh_rate_controls)
                         ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 }
