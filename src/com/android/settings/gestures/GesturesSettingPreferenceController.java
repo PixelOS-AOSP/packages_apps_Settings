2f8eccf4acd0eb7e8fc8554ece0a2edf5d76ab68
Cosmin Tanislav <demonsingur@gmail.com>
2022-06-23 01:44:32 +0300

    Settings: gestures: Add page for Screen-Off UDFPS
    Change-Id: I313969384fe605d7b5ff7dfc3a3fcebd514dd7cf
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- src/com/android/settings/gestures/GesturesSettingPreferenceController.java
+++ src/com/android/settings/gestures/GesturesSettingPreferenceController.java
@@ -79,6 +79,8 @@ public class GesturesSettingPreferenceController extends BasePreferenceControlle
         controllers.add(new PreventRingingParentPreferenceController(context, FAKE_PREF_KEY));
         controllers.add(new SwipeToScreenshotPreferenceController(context, FAKE_PREF_KEY));
         controllers.add(new PowerButtonTorchGesturePreferenceController(context, FAKE_PREF_KEY));
+        controllers.add(new ScreenOffUdfpsPreferenceController(context, FAKE_PREF_KEY)
+                .setConfig(ambientDisplayConfiguration));
         return controllers;
     }
 }
