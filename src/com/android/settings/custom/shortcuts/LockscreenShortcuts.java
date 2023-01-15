966097c8b5d9e7551f24a8a53c3d3586357f282a
Ido Ben-Hur <idoybh2@gmail.com>
2023-01-15 15:04:20 +0100

    Settings: Allow setting bottom keyguard shortcuts [2/3]
    Change-Id: Ib19a4e8ded61d4f593693b636b94566d96b457ae

---
+++ src/com/android/settings/custom/shortcuts/LockscreenShortcuts.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright (C) 2017 AospExtended ROM Project
+ * Copyright (C) 2023 DerpFest
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.custom.shortcuts;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SearchIndexable
+public class LockscreenShortcuts extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    private static final String SHORTCUT_START_KEY = "lockscreen_shortcut_start";
+    private static final String SHORTCUT_END_KEY = "lockscreen_shortcut_end";
+
+    private static final String[] DEFAULT_START_SHORTCUT = new String[] { "home", "flashlight" };
+    private static final String[] DEFAULT_END_SHORTCUT = new String[] { "wallet", "qr", "camera" };
+
+    private ListPreference mStartShortcut;
+    private ListPreference mEndShortcut;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.lockscreen_shortcuts);
+
+        mStartShortcut = (ListPreference) findPreference(SHORTCUT_START_KEY);
+        mEndShortcut = (ListPreference) findPreference(SHORTCUT_END_KEY);
+        updateShortcutSelection();
+        mStartShortcut.setOnPreferenceChangeListener(this);
+        mEndShortcut.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mStartShortcut) {
+            setShortcutSelection((String) newValue, true);
+            return true;
+        } else if (preference == mEndShortcut) {
+            setShortcutSelection((String) newValue, false);
+            return true;
+        }
+        return false;
+    }
+
+    private String getSettingsShortcutValue() {
+        String value = Settings.System.getString(getActivity().getContentResolver(),
+                Settings.System.KEYGUARD_QUICK_TOGGLES);
+        if (value == null || value.isEmpty()) {
+            for (String str : DEFAULT_START_SHORTCUT) value += str;
+            value += ";";
+            for (String str : DEFAULT_END_SHORTCUT) value += str;
+        }
+        return value;
+    }
+
+    private void updateShortcutSelection() {
+        final String value = getSettingsShortcutValue();
+        final String[] split = value.split(";");
+        mStartShortcut.setValue(split[0].split(",")[0]);
+        mStartShortcut.setSummary(mStartShortcut.getEntry());
+        mEndShortcut.setValue(split[1].split(",")[0]);
+        mEndShortcut.setSummary(mEndShortcut.getEntry());
+    }
+
+    private void setShortcutSelection(String value, boolean start) {
+        final String oldValue = getSettingsShortcutValue();
+        final int splitIndex = start ? 0 : 1;
+        String[] split = oldValue.split(";");
+        if (value.equals("none")) {
+            split[splitIndex] = "none";
+        } else {
+            split[splitIndex] = value;
+            final String[] def = start ? DEFAULT_START_SHORTCUT : DEFAULT_END_SHORTCUT;
+            for (String str : def) {
+                if (str.equals(value)) continue;
+                split[splitIndex] += "," + str;
+            }
+        }
+        Settings.System.putString(getActivity().getContentResolver(),
+                Settings.System.KEYGUARD_QUICK_TOGGLES, split[0] + ";" + split[1]);
+
+        if (start) {
+            mStartShortcut.setValue(value);
+            mStartShortcut.setSummary(mStartShortcut.getEntry());
+        } else {
+            mEndShortcut.setValue(value);
+            mEndShortcut.setSummary(mEndShortcut.getEntry());
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.CUSTOM;
+    }
+
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+        new BaseSearchIndexProvider() {
+            @Override
+            public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                    boolean enabled) {
+                final ArrayList<SearchIndexableResource> result = new ArrayList<>();
+                final SearchIndexableResource sir = new SearchIndexableResource(context);
+                sir.xmlResId = R.xml.lockscreen_shortcuts;
+                result.add(sir);
+                return result;
+            }
+
+            @Override
+            public List<String> getNonIndexableKeys(Context context) {
+                final List<String> keys = super.getNonIndexableKeys(context);
+                return keys;
+            }
+    };
+}
