120a601b1b0b9340b488c78fbafa16d61171ba52
Ido Ben-Hur <idoybh2@gmail.com>
2023-04-11 17:06:18 +0300

    Settings: Convert DoubleTap and PickupGesture to PrimarySwitchPreference
    - This is done to prepare for the upcoming commit.
    
    [cyberknight777]: Remove redundant VideoPreference cast.
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- src/com/android/settings/gestures/DoubleTapScreenSettings.java
+++ src/com/android/settings/gestures/DoubleTapScreenSettings.java
@@ -27,8 +27,11 @@ import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.dashboard.suggestions.SuggestionFeatureProvider;
 import com.android.settings.overlay.FeatureFactory;
 import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.search.SearchIndexable;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -47,9 +50,18 @@ public class DoubleTapScreenSettings extends DashboardFragment {
                 .getSuggestionFeatureProvider(context);
         SharedPreferences prefs = suggestionFeatureProvider.getSharedPrefs(context);
         prefs.edit().putBoolean(PREF_KEY_SUGGESTION_COMPLETE, true).apply();
+    }
 
-        use(DoubleTapScreenPreferenceController.class)
-                .setConfig(new AmbientDisplayConfiguration(context));
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context, getSettingsLifecycle());
+    }
+
+    private static List<AbstractPreferenceController> buildPreferenceControllers(Context context,
+            Lifecycle lifecycle) {
+        List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new DoubleTapPreferenceController(context));
+        return controllers;
     }
 
     @Override
